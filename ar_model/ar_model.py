8cccc6ff95c24e60ed25f133563bda6d9dde1006
Tensor <narutokelechi@gmail.com>
2024-09-29 14:10:13 +0100

    [add] encodec and llama model loading/configs

---
+++ ar_model/ar_model.py
@@ -0,0 +1,33 @@
+from transformers import (
+    AutoTokenizer,
+    EncodecModel,
+    AutoProcessor,
+    LlamaModel,
+    LlamaConfig,
+)
+from .ar_utils import model_configs
+
+# Audio encoder, FaceBook Encodec-32khz
+encodec_model = EncodecModel.from_pretrained(model_configs.encodec_id)
+
+audio_processor = AutoProcessor.from_pretrained(
+    model_configs.encodec_id
+)  # preprocessor for neural audio codec
+
+# freeze or prevent gradient update
+encodec_model.eval()
+
+# LLM tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    model_configs.llama_id,
+)
+# Llama model architecture, for initial experiments
+llama_config = LlamaConfig(
+    num_attention_heads=8,
+    num_hidden_layers=16,
+    num_key_value_heads=4,
+    hidden_size=2048,
+    intermediate_size=8192,
+    head_dim=32,
+)
+tiny_llama = LlamaModel(config=llama_config)
